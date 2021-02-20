e6a68c0f4357dd12f7a7392e28d4d1cf582458ac
rockthrone <adam@xstreamtrucking.com>
2021-02-19 19:02:13 -0800

    logfind.c will print each line with target word in it Added .logfind file

--- logfind.c
+++ logfind.c
@@ -1,20 +1,48 @@
+/*
+use scan_file to go line by line 
+and use strcasestr to see if the 
+line contains the term
+*/
 
-
-#include <stdio.h>
 #include "dbg.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_LINE 1024
+
+int scan_file (const char *filename, int search_len, char *search_for[])
+{
+    char *line = calloc(MAX_LINE, 1); 
+    FILE *file = fopen(filename, "r");
+    while(fgets(line, MAX_LINE, file) != NULL){  
+        if (strcasestr(line, "international") != NULL){
+            printf("%s",line);
+        }
+    }
+    
+    return 0;
+}
 
 int main (int argc, char *argv[])
 {
-    // Count number of inputs 
-    int val_count = 0;
-    for (int i = 0; argv[i] != NULL; i++){
-        val_count++;
-    }
-    debug("val_count: %d", val_count);
+    int rc = 0;
 
+    rc = scan_file("un_preamble.txt", argc, argv);
+    
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
