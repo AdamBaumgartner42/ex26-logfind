62cb248edf4235388116e3267e97be3039e24ccc
rockthrone <adam@xstreamtrucking.com>
2021-02-19 19:49:38 -0800

    Reading multiple values from single file:w

--- logfind.c
+++ logfind.c
@@ -15,9 +15,23 @@ int scan_file (const char *filename, int search_len, char *search_for[])
 {
     char *line = calloc(MAX_LINE, 1); 
     FILE *file = fopen(filename, "r");
-    while(fgets(line, MAX_LINE, file) != NULL){  
-        if (strcasestr(line, "international") != NULL){
-            printf("%s",line);
+    int match_count = 0;
+
+    //debug("search_len: %d", search_len);
+
+    while(fgets(line, MAX_LINE, file) != NULL){ 
+        int match_count = 0; // reset every line
+        for (int i = 1; i < search_len; i++){
+            if (strcasestr(line, search_for[i]) != NULL){ 
+                //debug("%s ", search_for[i]); 
+                match_count++;  // 
+            } 
+            if (match_count == search_len - 1){ // if there is a match, it increments the count 
+                printf("%s",line);
+            }
+            if (match_count > 0){
+                //debug("match_count: %d", match_count);
+            }
         }
     }
     
