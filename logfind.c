8893332162a73348ad74c9781b2f426805b84933
rockthrone <adam@xstreamtrucking.com>
2021-02-26 21:58:17 -0800

    reads files from .logfind file. "-o" can be used to signal OR in comparison.

--- logfind.c
+++ logfind.c
@@ -18,9 +18,11 @@ int scan_file (const char *filename, int search_len, char **search_for, int *wor
 
     while(fgets(line, MAX_LINE, file) != NULL){ 
         for (int i = 1; i < search_len; i++){
-            if (strcasestr(line, search_for[i]) != NULL){
-                word_count[i]++; 
-            } 
+            if(strcmp(search_for[i],"-o") != 0){
+                if (strcasestr(line, search_for[i]) != NULL){
+                    word_count[i]++; 
+                }
+            }
         }
     }
 
@@ -73,19 +75,27 @@ error:
     return -1;
 }
 
-int judge_results (int *array, int len, char *filename)
+int judge_results (int *array, int len, char *filename, int or_status)
 {
+    int i; 
     int found_count = 0;
     int search_count = len - 1;
-    for (int i = 1; i < len; i++){
+
+    for (i = 1; i < len; i++){
         if(array[i] > 0){
             found_count++; 
         }
+    }
+
+    if(or_status == 1 && found_count > 0){ 
+        printf("%s\n", filename);
+        return 0;
     }
     if(found_count == search_count){
         printf("%s\n", filename);
         return 0;
     }
+
     return 0;
 
 error:
@@ -94,36 +104,50 @@ error:
 
 int find_files(glob_t *pglob)
 {
+    char *line = calloc(MAX_LINE, 1);
+    FILE *file = fopen(".logfind", "r");
     int glob_flags = GLOB_TILDE;
-    // glob_flags =  100000000000 
-    glob("*.c", glob_flags, NULL, pglob); //  100000000000
-    glob_flags = glob_flags | GLOB_APPEND; // bitwise OR  
-    // glob_flags = 100000000001 , see the new '1' at the end? 
-    glob("*.h", glob_flags, NULL, pglob); 
+   
+    while(fgets(line, MAX_LINE, file) != NULL){
+        line[strlen(line) - 1] = '\0'; 
+        glob(line, glob_flags, NULL, pglob); 
     
+        if(glob_flags == GLOB_TILDE){
+            glob_flags = glob_flags | GLOB_APPEND;
+        }
+            
+    } 
     return 0; 
     
 error:
     return -1;
 }
 
-
+// error if or is used and NONE of the words are present. 
+// logfind is being selected, every time 
 
 int main (int argc, char *argv[])
 {
-    int word_count[argc]; //count status
+    int i; 
+    int or_flag = 0;
+    int word_count[argc]; 
     check(argc > 1, "USAGE: logfind word word word");
     
-
-    // Use glob to find files
+    // Check for '-o' in argv[]
+    for (i = 1; i < argc; i++){
+        if (strcmp(argv[i], "-o") == 0){ // always returning true
+            or_flag = 1;
+        }
+    }
+    
     glob_t found_files;
     find_files(&found_files);
 
     // Search across the relevant files 
-    for (int i = 0; i < found_files.gl_pathc; i++){
+    for (i = 0; i < found_files.gl_pathc; i++){
         zero_array(word_count, argc);  
         scan_file(found_files.gl_pathv[i], argc, argv, word_count); 
-        judge_results(word_count, argc,found_files.gl_pathv[i]);
+        judge_results(word_count, argc,found_files.gl_pathv[i], or_flag);
     }
 
     return 0;
